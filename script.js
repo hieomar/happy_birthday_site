b935963b20a980182cb3695dae0a60aca248499d
omar chibwana <134367789+hieomar@users.noreply.github.com>
2023-08-07 08:47:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function addBirthdayWish() {
+    const messageList = document.getElementById('message-list');
+    const birthdayWish = document.getElementById('birthday-wish').value;
+
+    if (birthdayWish.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = birthdayWish;
+        messageList.appendChild(li);
+        document.getElementById('birthday-wish').value = '';
+    }
+}
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".memory-lane .slide");
+
+function showSlides() {
+    slides[slideIndex].style.display = "none"; // Hide the current slide
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0; // Reset the index if all slides are shown
+    }
+    slides[slideIndex].style.display = "block"; // Show the next slide
+    setTimeout(showSlides, 5000); // Change slide every 3 seconds
+}
+
+showSlides();
